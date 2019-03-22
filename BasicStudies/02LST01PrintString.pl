6de8047a6d1b32f30dff06fea7cf7a6d13e75153
rduvalwa5 <rduvalwa5@hotmail.com>
2019-03-21 18:33:10 -0700

    add first code

---
+++ BasicStudies/02LST01PrintString.pl
@@ -0,0 +1,20 @@
+print 'Bill of goods
+
+Bread: $24.45
+Fruits: $40.00
+       =======
+        $64.45
+
+OSXAir:PerlStudies rduvalwa2$ cd BasicStudies/
+OSXAir:BasicStudies rduvalwa2$ ls
+02LST01PrintString.pl
+OSXAir:BasicStudies rduvalwa2$ perl 02LST01PrintString.pl 
+Bill of goods
+
+Bread: $24.45
+Fruits: $40.00
+       =======
+        $64.45
+OSXAir:BasicStudies rduvalwa2$ 
+
+';
