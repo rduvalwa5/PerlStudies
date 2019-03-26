f17441174145a2a954423c06e9d02a5c9dfb2b55
rduvalwa5@hotmail.com <rduvalwa5@hotmail.com>
2019-03-26 12:34:21 -0700

    add code and notes

---
+++ BasicStudies/arrayVarible.pl
@@ -0,0 +1,23 @@
+#Here is a simple example of using array variables −
+#!/usr/bin/perl
+
+@ages = (25, 30, 40);             
+@names = ("John Paul", "Lisa", "Kumar");
+
+print "\$ages[0] = $ages[0]\n";
+print "\$ages[1] = $ages[1]\n";
+print "\$ages[2] = $ages[2]\n";
+print "\$names[0] = $names[0]\n";
+print "\$names[1] = $names[1]\n";
+print "\$names[2] = $names[2]\n";
+
+=Begin
+this will produce the following result −
+
+$ages[0] = 25
+$ages[1] = 30
+$ages[2] = 40
+$names[0] = John Paul
+$names[1] = Lisa
+$names[2] = Kumar
+=cut
