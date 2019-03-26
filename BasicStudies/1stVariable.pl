8293c7d1d2a4e6bce6d9ca3fd6020e6fec93a9f5
rduvalwa5 <rduvalwa5@hotmail.com>
2019-03-25 23:45:37 -0700

    add notes and learning scripts

---
+++ BasicStudies/1stVariable.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+$a = 10;
+print "Value of a = $a note this is the variable\n";
+print 'Value of a = $a\n Note this is a literal';
+
+=begin
+OSXAir:BasicStudies rduvalwa2$ perl 1stVariable.pl 
+Value of a = 10
+Value of a = $a\nOSXAir:BasicStudies rduvalwa2$ 
+=cut
---
+++ BasicStudies/HelloWorldScript.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+# This will print "Hello, World"
+print "Hello, world\n";
+print ("This can be run as a shell script ./HelloWorldScript.sh\n");
+
+=begin comment
+OSXAir:BasicStudies rduvalwa2$ ./HelloWorldScript.sh
+Hello, world
+This can be run as a shell script ./HelloWorldScript.sh
+OSXAir:BasicStudies rduvalwa2$ 
+
+This is all part of multiline comment.
+You can use as many lines as you like
+These comments will be ignored by the 
+compiler until the next =cut is encountered.
+=cut
